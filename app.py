569dd8792c933d3eeceaafc25496682f39d1bb91
pshemach <pasindusandakan@gmail.com>
2025-04-20 14:24:13 +0530

    port changes

--- app.py
+++ app.py
@@ -319,4 +319,4 @@ def get_job(job_id):
     return jsonify(job_info)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5096)
